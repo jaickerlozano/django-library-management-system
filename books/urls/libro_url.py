8de6adcbf4a7e1f2bdb947729c5906337f95eb8c
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-03-01 23:04:27 -0300

    Refactor(URLs): Modularizar configuración de rutas para libros y autores

---
+++ books/urls/libro_url.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from books.views import LibroListView, LibroDetailView, LibroCreateView, LibroUpdateView, LibroDeleteView
+
+
+app_name = 'libro'
+
+urlpatterns = [
+    path('list/', LibroListView.as_view(), name='list'),
+    path('detail/<pk>/', LibroDetailView.as_view(), name='detail'),
+    path('create/', LibroCreateView.as_view(), name='create'),
+    path('update/<pk>/', LibroUpdateView.as_view(), name='update'),
+    path('delete/<pk>/', LibroDeleteView.as_view(), name='delete'),
+]

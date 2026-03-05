4e9e5d3470e2a239b813d159c1f46d138e43f348
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-03-04 23:32:50 -0300

    feat(editoriales): implementar CRUD con restricción de permisos y owner

--- books/views/editoriales_views.py
+++ books/views/editoriales_views.py
@@ -5,6 +5,11 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from books.decorators import user_can_delete_editorial
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 # Create your views here.
 class EditorialListView(ListView):
@@ -18,27 +23,33 @@ class EditorialDetailView(DetailView):
     template_name = 'editoriales/EditorialDetail.html'
     context_object_name = 'editorial'
 
-
-class EditorialCreateView(CreateView):
+@method_decorator(login_required, name="dispatch") # -> Este decorador bloquea la vista para crear editoriales a menos que se esté logueado
+class EditorialCreateView(SuccessMessageMixin, CreateView):
     model = Editorial
+    form_class = EditorialModelFormCreate
     template_name = 'editoriales/EditorialCreate.html'
     success_url = reverse_lazy('editorial:list')
-    fields = [
-        'nombre', 'direccion', 'ciudad', 'estado', 'pais', 'codigo_postal', 'telefono', 'email', 'sitio_web', 'fecha_fundacion',
-    ]
+    success_message = 'La editorial se ha creado correctamente'
 
+    def form_valid(self, form):
+        # Antes de guardar el formulario, asignamos el usuario logueado
+        form.instance.created_by = self.request.user
+        
+        # Continuamos con el comportamiento normal (guardar y redireccionar)
+        return super().form_valid(form)
 
-class EditorialUpdateView(UpdateView):
+@method_decorator(user_can_delete_editorial, name="dispatch")
+class EditorialUpdateView(SuccessMessageMixin, UpdateView):
     model = Editorial
+    form_class = EditorialModelFormCreate
     template_name = 'editoriales/EditorialUpdate.html'
     success_url = reverse_lazy('editorial:list')
-    fields = [
-        'nombre', 'direccion', 'ciudad', 'estado', 'pais', 'codigo_postal', 'telefono', 'email', 'sitio_web', 'fecha_fundacion',
-    ]
+    success_message = 'La editorial se ha actualizado correctamente'
 
-
-class EditorialDeleteView(DeleteView):
+@method_decorator(user_can_delete_editorial, name="dispatch")
+class EditorialDeleteView(SuccessMessageMixin, DeleteView):
     model = Editorial
     template_name = 'editoriales/EditorialDelete.html'
     success_url = reverse_lazy('editorial:list')
+    success_message = 'La editorial se ha eliminado correctamente'
 

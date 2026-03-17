3e6f95475a311796d9fd4128397ea413966fbc8c
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-03-17 10:56:00 -0300

    test(i18n): agregar pruebas para validar traducción de modelos y etiquetas

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.utils import translation
+from books.models import Autor, Editorial, Libro
+from django.urls import reverse
 
-# Create your tests here.
+class TranslationTest(TestCase):
+    def test_model_verbose_name_translation(self):
+        # Default (Spanish)
+        with translation.override('es'):
+            self.assertEqual(str(Autor._meta.verbose_name), "Autor")
+            self.assertEqual(str(Autor._meta.verbose_name_plural), "Autores")
+            self.assertEqual(str(Editorial._meta.verbose_name), "Editorial")
+            self.assertEqual(str(Libro._meta.verbose_name), "Libro")
+
+        # English
+        with translation.override('en'):
+            self.assertEqual(str(Autor._meta.verbose_name), "Author")
+            self.assertEqual(str(Autor._meta.verbose_name_plural), "Authors")
+            self.assertEqual(str(Editorial._meta.verbose_name), "Publisher")
+            self.assertEqual(str(Libro._meta.verbose_name), "Book")
+
+    def test_field_label_translation(self):
+        # Autor fields
+        with translation.override('en'):
+            self.assertEqual(str(Autor._meta.get_field('nombre').verbose_name), "Name")
+            self.assertEqual(str(Autor._meta.get_field('apellido').verbose_name), "Last Name")
+            self.assertEqual(str(Autor._meta.get_field('fecha_nacimiento').verbose_name), "Birth Date")
+
+        # Libro fields
+        with translation.override('en'):
+            self.assertEqual(str(Libro._meta.get_field('titulo').verbose_name), "Title")
+            self.assertEqual(str(Libro._meta.get_field('numero_paginas').verbose_name), "Number of pages")
+
+    def test_view_message_translation(self):
+        # We don't necessarily need to test the whole view flow here, 
+        # but we could if we had the necessary setup (user, etc.)
+        pass
